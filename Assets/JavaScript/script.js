0acec48bae3892e6051f5b8a6998a9ca0a86a4e0
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-14 18:36:13 -0500

    pushing updated JS code

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,10 +1,12 @@
 let weatherAPIKey = 'caaae4a391468490b870f3bb48d1aa3d';
 let geocoding = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
-let weatherChecker = 'https://api.openweathermap.org/data/2.5/weather?appid=' + weatherAPIKey;
+let weatherChecker = 'https://api.openweathermap.org/data/2.5/weather?';
 
 let getWeatherByCityName = async (city) => {
-    let endpoint = weatherChecker + 'q=' + city;
-    let responce = awate fetch(endpoint);
+    let endpoint = weatherChecker + 'q=' + city + weatherAPIKey;
+    let responce = await fetch(endpoint);
 }
 
 console.log(responce);
+
+getWeatherByCityName('london');
