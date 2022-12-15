65940db2691385ead8dd6fae26102857c4e0078b
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-14 20:26:30 -0500

    Added query selectors and trying to link search bar to function

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,16 +1,34 @@
+// Below are my query selectors
+let searchInp = document.querySelector('.weather-search');
+let city = document.querySelector('.weather-city');
+let day = document.querySelector('.weather-day');
+let humidity = document.querySelector('.humidity>.value');
+let wind = document.querySelector('.wind>.value');
+let pressure = document.querySelector('.pressure>.value');
+let image = document.querySelector('.weather-img')
+let temperature = document.querySelector('.temperature');
+
+// Below are my API selectors
 let weatherAPIKey = 'caaae4a391468490b870f3bb48d1aa3d';
-let geocoding = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
-let weatherChecker = 'https://api.openweathermap.org/data/2.5/weather?appid' + weatherAPIKey;
+// let geocoding = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+let weatherChecker = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + weatherAPIKey;
 
 let getWeatherByCityName = async (city) => {
     let endpoint = weatherChecker + '&q=' + city;
-    let responce = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=london&appid=caaae4a391468490b870f3bb48d1aa3d');
+    let responce = await fetch(endpoint);
     let weather = await responce.json();
+    return weather;
     
-    console.log(weather);
 }
 
-getWeatherByCityName('london');
+getWeatherByCityName('New York');
+
+searchInp.addEventListener('keydown', async (e) => {
+if(e.KeyCode === 13) {
+    let weather = await getWeatherByCityName(searchInp.value);
+    console.log(weather);
+}
+})
 
 
 // When A user enters a city name into the search Bar
